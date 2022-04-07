0af44c4e0316b3be9392bddaa5cda64f59dbbf6c
Nam Anh <phamnamanh25@gmail.com>
2022-04-07 21:46:45 +0700

    features, refactor, eslint and more

--- src/commands.ts
+++ src/commands.ts
@@ -4,37 +4,64 @@ import { Routes } from "discord-api-types/v9";
 import { clientId, token, guildId } from "./config.json";
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName("reminder")
-    .setDescription("Make a new reminder message")
-    .addStringOption((option) =>
-      option
-        .setName("message")
-        .setDescription("Reminder message content")
-        .setRequired(true)
-    ),
-  new SlashCommandBuilder()
-    .setName("edit")
-    .setDescription("Edit a reminder message")
-    .addStringOption((option) =>
-      option
-        .setName("id")
-        .setDescription("Reminder message ID")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("pastebin")
-        .setDescription("Pastebin URL for reminder message content")
-        .setRequired(true)
-    ),
-].map((command) => command.toJSON());
+	new SlashCommandBuilder()
+		.setName("make")
+		.setDescription("Make a new reminder message")
+		.addStringOption(option =>
+			option
+				.setName("message")
+				.setDescription("(Pastebin URL containing) Reminder message content")
+				.setRequired(true)
+		)
+		.addBooleanOption(option =>
+			option
+				.setName("mention")
+				.setDescription("Mention everyone after the message is created")
+				.setRequired(false)
+		)
+		.addStringOption(option =>
+			option
+				.setName("note")
+				.setDescription(
+					"Note in the mention message (useless if 'mention' is false)"
+				)
+				.setRequired(false)
+		),
+	new SlashCommandBuilder()
+		.setName("edit")
+		.setDescription("Edit a reminder message")
+		.addStringOption(option =>
+			option
+				.setName("id")
+				.setDescription("Reminder message ID")
+				.setRequired(true)
+		)
+		.addStringOption(option =>
+			option
+				.setName("message")
+				.setDescription("(Pastebin URL containing) Reminder message content")
+				.setRequired(true)
+		)
+		.addBooleanOption(option =>
+			option
+				.setName("mention")
+				.setDescription("Mention everyone after the message is created")
+				.setRequired(false)
+		)
+		.addStringOption(option =>
+			option
+				.setName("note")
+				.setDescription(
+					"Note in the mention message (useless if 'mention' is false)"
+				)
+				.setRequired(false)
+		)
+].map(command => command.toJSON());
 
-const rest = new REST({ version: "9" }).setToken(token);
-
-rest
-  .put(Routes.applicationGuildCommands(clientId, guildId), {
-    body: commands,
-  })
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+new REST({ version: "9" })
+	.setToken(token)
+	.put(Routes.applicationGuildCommands(clientId, guildId), {
+		body: commands
+	})
+	.then(() => console.log("Successfully registered application commands."))
+	.catch(console.error);
--- src/index.ts
+++ src/index.ts
@@ -5,80 +5,110 @@ import { token } from "./config.json";
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once("ready", () => {
-  client.user!.setPresence({
-    activities: [{ name: "your mom" }],
-    status: "idle",
-  });
-  console.log("Ready!");
+	client.user.setPresence({
+		activities: [{ name: "your mom" }],
+		status: "idle"
+	});
+	console.log("Ready!");
 });
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-  console.log(interaction);
+client.on("interactionCreate", async interaction => {
+	if (!interaction.isCommand()) return;
 
-  const { commandName, member, options } = interaction;
+	const { commandName, member, options, user, channel } = interaction;
 
-  switch (commandName) {
-    case "reminder":
-      if (
-        !(member.roles as GuildMemberRoleManager).cache.some(
-          (role) => role.name === "DaCoolCorp™"
-        )
-      ) {
-        await interaction.reply({ content: "lmao no perms", ephemeral: true });
-      } else if (interaction.channelId !== "785492392606040094") {
-        await interaction.reply({
-          content: "wrong channel buffoon",
-          ephemeral: true,
-        });
-      } else {
-        // For later when am smart
-        /* const reminderEmbed = new MessageEmbed()
-          .setColor(user.hexAccentColor)
-          .setAuthor({
-            name: `Updated by: ${user.tag}`,
-            iconURL: user.avatarURL({ size: 4096 }),
-          })
-          .setTitle("DaCoolReminder™")
-          .setDescription(
-            `as of ${today.getDate()} / ${today.getMonth()} / ${today.getFullYear()}`
-          )
-          .setTimestamp(); */
-        const messageContent = options.getString("message");
-        await interaction.reply(messageContent);
-      }
-      break;
-    case "edit": {
-      const id = options.getString("id");
-      const pastebin = options.getString("pastebin").split("/");
-      const editMessage = interaction.channel.messages.fetch(id);
-      if (!editMessage) {
-        await interaction.reply({
-          content: "Reminder message with given ID is not found",
-          ephemeral: true,
-        });
-      } else {
-        axios
-          .get(`https://pastebin.com/raw/${pastebin[pastebin.length - 1]}`)
-          .then(async (response) => {
-            console.log(response.data);
-            (await editMessage).edit(response.data);
-            await interaction.reply({
-              content: "Successfully edited reminder message",
-              ephemeral: true,
-            });
-          })
-          .catch(async (error) => {
-            console.log(error);
-            await interaction.reply({
-              content:
-                "Something was messed up, double check your Pastebin URL",
-              ephemeral: true,
-            });
-          });
-      }
-    }
-  }
+	switch (commandName) {
+		case "make":
+			if (
+				(member.roles as GuildMemberRoleManager).cache.some(
+					role => role.name === "DaCoolCorp™"
+				) ||
+				user.id === "368399721494216706"
+			) {
+				const message = options.getString("message"),
+					note = options.getString("note");
+				if (message.match(/^(http(s)?[:][\\/][\\/])?pastebin[.]com/gm)) {
+					const pastebin = message.split("/");
+					axios
+						.get(`https://pastebin.com/raw/${pastebin[pastebin.length - 1]}`)
+						.then(async response => {
+							await interaction.reply(response.data);
+						})
+						.catch(async error => {
+							console.log(error);
+							await interaction.reply({
+								content:
+									"Something was messed up, double check your Pastebin URL",
+								ephemeral: true
+							});
+						});
+				} else await interaction.reply(message);
+
+				if (options.getBoolean("mention")) {
+					let mentionMsg = `@everyone DaCoolReminder is updated for ${new Date().toLocaleString(
+						"en-US",
+						{ weekday: "long", month: "long", day: "numeric" }
+					)}`;
+					if (note) mentionMsg = `${mentionMsg} (${note})`;
+
+					await channel.send(mentionMsg);
+				}
+			} else
+				await interaction.reply({ content: "lmao no perms", ephemeral: true });
+			break;
+		case "edit":
+			if (
+				(member.roles as GuildMemberRoleManager).cache.some(
+					role => role.name === "DaCoolCorp™"
+				) ||
+				user.id === "368399721494216706"
+			) {
+				const message = options.getString("message"),
+					editMessage = await channel.messages.fetch(options.getString("id")),
+					note = options.getString("note");
+				if (!editMessage) {
+					await interaction.reply({
+						content: "Reminder message with given ID is not found",
+						ephemeral: true
+					});
+				} else if (message.match(/^(http(s)?[:][\\/][\\/])?pastebin[.]com/gm)) {
+					const pastebin = message.split("/");
+					axios
+						.get(`https://pastebin.com/raw/${pastebin[pastebin.length - 1]}`)
+						.then(async response => {
+							editMessage.edit(response.data);
+							await interaction.reply({
+								content: "Successfully edited reminder message",
+								ephemeral: true
+							});
+						})
+						.catch(async error => {
+							console.log(error);
+							await interaction.reply({
+								content:
+									"Something was messed up, double check your Pastebin URL",
+								ephemeral: true
+							});
+						});
+				} else {
+					await editMessage.edit(message);
+					await interaction.reply({
+						content: "Successfully edited reminder message",
+						ephemeral: true
+					});
+				}
+				if (options.getBoolean("mention")) {
+					let mentionMsg = `@everyone DaCoolReminder is updated for ${new Date().toLocaleString(
+						"en-US",
+						{ weekday: "long", month: "long", day: "numeric" }
+					)}`;
+					if (note) mentionMsg = `${mentionMsg} (${note})`;
+
+					await channel.send(mentionMsg);
+				}
+			} else
+				await interaction.reply({ content: "lmao no perms", ephemeral: true });
+	}
 });
 
 client.login(token);
