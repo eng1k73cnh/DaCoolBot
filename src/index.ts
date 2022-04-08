2d2360ed066212702fd116c5f230ab690042028f
Nam Anh <phamnamanh25@gmail.com>
2022-04-08 15:53:55 +0700

    test: refactor promise callback

--- src/index.ts
+++ src/index.ts
@@ -65,39 +65,46 @@ client.on("interactionCreate", async interaction => {
 				user.id === "368399721494216706"
 			) {
 				const message = options.getString("message"),
-					editMessage = await channel.messages.fetch(options.getString("id")),
 					note = options.getString("note");
-				if (!editMessage) {
-					await interaction.reply({
-						content: "Reminder message with given ID is not found",
-						ephemeral: true
-					});
-				} else if (message.match(/^(http(s)?[:][\\/][\\/])?pastebin[.]com/gm)) {
-					const pastebin = message.split("/");
-					axios
-						.get(`https://pastebin.com/raw/${pastebin[pastebin.length - 1]}`)
-						.then(async response => {
-							editMessage.edit(response.data);
+				await channel.messages
+					.fetch(options.getString("id"))
+					.then(async editMessage => {
+						if (message.match(/^(http(s)?[:][\\/][\\/])?pastebin[.]com/gm)) {
+							const pastebin = message.split("/");
+							axios
+								.get(
+									`https://pastebin.com/raw/${pastebin[pastebin.length - 1]}`
+								)
+								.then(async response => {
+									editMessage.edit(response.data);
+									await interaction.reply({
+										content: "Successfully edited reminder message",
+										ephemeral: true
+									});
+								})
+								.catch(async error => {
+									console.log(error);
+									await interaction.reply({
+										content:
+											"Something was messed up, double check your Pastebin URL",
+										ephemeral: true
+									});
+								});
+						} else {
+							await editMessage.edit(message);
 							await interaction.reply({
 								content: "Successfully edited reminder message",
 								ephemeral: true
 							});
-						})
-						.catch(async error => {
-							console.log(error);
-							await interaction.reply({
-								content:
-									"Something was messed up, double check your Pastebin URL",
-								ephemeral: true
-							});
+						}
+					})
+					.catch(async error => {
+						console.log(error);
+						await interaction.reply({
+							content: "Reminder message with given ID is not found",
+							ephemeral: true
 						});
-				} else {
-					await editMessage.edit(message);
-					await interaction.reply({
-						content: "Successfully edited reminder message",
-						ephemeral: true
 					});
-				}
 				if (options.getBoolean("mention")) {
 					let mentionMsg = `@everyone DaCoolReminder is updated for ${new Date().toLocaleString(
 						"en-US",
