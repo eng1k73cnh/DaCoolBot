7c40d1b0d7368937c415c2da15807f5345cd777d
Nam Anh <phamnamanh25@gmail.com>
2022-04-08 22:21:55 +0700

    fix: command not responding

--- src/index.ts
+++ src/index.ts
@@ -25,4 +25,27 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
+client.on("interactionCreate", async interaction => {
+	interface command {
+		execute(interaction);
+	}
+
+	if (!interaction.isCommand()) return;
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const command: any = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({
+			content: "There was an error while executing this command!",
+			ephemeral: true
+		});
+	}
+});
+
 client.login(process.env.TOKEN);
