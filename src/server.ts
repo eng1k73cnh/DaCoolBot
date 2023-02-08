a5c6b32d43af6f666f477ba4e65e25f4b7b0b740
Nam Anh <phamnamanh25@gmail.com>
2023-02-08 20:22:17 +0700

    fix: allow CORS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import express from "express";
+import cors from "cors";
 
 // Own API
 const app = express();
+
+// Allow CORS
+app.use(cors());
 
 app.get("/", (req, res) => {
 	const rest = new REST({ version: "9" }).setToken(process.env.TOKEN),
